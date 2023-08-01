d7e17c2d951c03646ce86ab357921893367f4982
Rob Ballantyne <rob@dynamedia.uk>
2023-08-01 08:05:16 +0100

    Structure template preflight.sh

--- build/COPY_ROOT/opt/ai-dock/bin/preflight.sh
+++ build/COPY_ROOT/opt/ai-dock/bin/preflight.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 # Extended image should have preflight.sh in $PATH before this one to modify startup behavior
+
+function main() {
+    do_something
+}
+
+function do_something() {
+    printf "Empty preflight.sh...\n"
+}
+
+main "$@"; exit
