1dc2a4906cddd3e1054eefdf282d2729d5eb1463
Rob Ballantyne <rob@dynamedia.uk>
2024-01-13 21:49:52 +0000

    allows easily passing:environment vatiables to supervisor-controlled processes

---
+++ build/COPY_ROOT/opt/ai-dock/bin/env-store.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Store environment variables so they can be used by supervisor processes
+key="$1"
+value="$(printenv $1)"
+code=$?
+
+if [[ -z $key ]]; then
+    printf "Usage: env-store key\n"
+    exit 1
+fi
+
+if [[ $code -ne 0 ]]; then
+    printf "Could not store key: %s\n" "$key"
+    exit 1
+fi
+
+printf "export %s=\"%s\"\n" "${key}" "${value}" >> /opt/ai-dock/etc/environment.sh
+printf "Stored environment variable '%s': %s\n" "$key" "$value"
