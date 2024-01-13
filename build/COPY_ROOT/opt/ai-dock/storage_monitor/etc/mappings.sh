847326f4be75b28364fc24b5e0973ba2497294e6
Rob Ballantyne <rob@dynamedia.uk>
2024-01-13 21:48:07 +0000

    support existing subdirectories in target directories

--- build/COPY_ROOT/opt/ai-dock/storage_monitor/etc/mappings.sh
+++ build/COPY_ROOT/opt/ai-dock/storage_monitor/etc/mappings.sh
@@ -2,6 +2,5 @@
 
 declare -A storage_map
 #storage_map["my/models"]="/opt/app_1/models /opt/app_2/models"
-#storage_map["my/datasets"]="/opt/app_1/datasets /opt/app_2/datasets"
 
 # Add more mappings for other repository directories as needed
