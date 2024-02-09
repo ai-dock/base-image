3f0768ff70002fd497e8546925ad5917214b0b4b
Rob Ballantyne <rob@dynamedia.uk>
2024-02-09 13:45:23 +0000

    Performance improvement for permissions fix

--- build/COPY_ROOT/opt/ai-dock/bin/fix-permissions.sh
+++ build/COPY_ROOT/opt/ai-dock/bin/fix-permissions.sh
@@ -19,10 +19,13 @@ function main() {
 
 function fix_container() {
     # Ensure we only affect files in this fs layer
+    printf "Fixing container file permissions...\n"
     chown root.ai-dock /opt
     chmod g+s /opt
     find /opt -not -group ai-dock -exec chown root.ai-dock {} \;
-    find /opt -type d ! -perm -g=w -exec chmod g+w {} \; -exec chmod g+s {} \;
+    find /opt -type d ! -perm -g=s -exec chmod g+s {} \;
+    find /opt ! -perm -g=w -exec chmod g+w {} \;
+    printf "Container file permissions reset\n"
 }
 
 function fix_workspace() {
@@ -30,15 +33,17 @@ function fix_workspace() {
         printf "Fixing workspace permissions...\n"
         chown "${WORKSPACE_UID}.${WORKSPACE_GID}" "${WORKSPACE}"
         chmod -R g+s "${WORKSPACE}"
-        find "${WORKSPACE}" -not -group "${WORKSPACE_GID}" -exec chown "${WORKSPACE_UID}.${WORKSPACE_GID}" {} \;
-        find "${WORKSPACE}" -type d ! -perm -g=w -exec chmod g+w {} \; -exec chmod g+s {} \;
+        find "${WORKSPACE}" -not -user "${WORKSPACE_UID}" -exec chown "${WORKSPACE_UID}.${WORKSPACE_GID}" {} \;
+        find "${WORKSPACE}" -type d ! -perm -g=s -exec chmod g+s {} \;
+        find "${WORKSPACE}" ! -perm -g=w -exec chmod g+w {} \;
         chmod o-rw "${WORKSPACE}/home/${USER_NAME}"
         if [[ -e ${WORKSPACE}/home/user/.ssh/authorized_keys ]]; then
             chmod 700 "${WORKSPACE}/home/${USER_NAME}/.ssh"
             chmod 600 "${WORKSPACE}/home/${USER_NAME}/.ssh/authorized_keys"
         fi
+        printf printf "Workspace file permissions reset\n"
     else
-        printf "No permissions changed (non-standard fs)\n"
+        printf "Workspace permissions not changed (non-standard fs)\n"
     fi
 }
 
