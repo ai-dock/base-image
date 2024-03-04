af12ac246387026d99c1ab3f381fccbcc873bffa
Rob Ballantyne <rob@dynamedia.uk>
2024-03-04 15:59:08 +0000

    Delay storagemonitor start until after workspace sync - Prevents race condition

--- build/COPY_ROOT/opt/ai-dock/bin/supervisor-storagemonitor.sh
+++ build/COPY_ROOT/opt/ai-dock/bin/supervisor-storagemonitor.sh
@@ -9,6 +9,13 @@ function cleanup() {
 function start() {
     source /opt/ai-dock/etc/environment.sh
     
+    if [[ -f /run/workspace_sync ]]; then
+        printf "Waiting for workspace sync to complete...\n"
+        while [[ -f /run/workspace_sync ]]; do
+            sleep 1
+        done
+    fi
+
     printf "Starting storage monitor..\n"
     /opt/ai-dock/storage_monitor/bin/storage-monitor.sh
 }
